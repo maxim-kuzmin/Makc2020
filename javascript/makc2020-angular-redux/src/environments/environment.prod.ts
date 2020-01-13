4a7a1b016d9ef2e5ca4c1883cd08675f2b924593
maxim.kuzmin <maxim.kuzmin@inbox.ru>
2020-01-13 12:14:31 +0300

    -

--- javascript/makc2020-angular-redux/src/environments/environment.prod.ts
+++ javascript/makc2020-angular-redux/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+const hostPort = 4201;
+
 export const environment = {
+  apiServerUrl: `http://localhost:5002`,
+  authTypeOidcIsEnabled: false,
+  identityServerUrl: `http://localhost:6002`,
+  hostPort: hostPort,
+  hostUrl: `http://localhost:${hostPort}`,
   production: true
 };
