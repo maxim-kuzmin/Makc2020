95ea7a6614f67306c73c18e683b5f04d4c137780
maxim.kuzmin <maxim.kuzmin@inbox.ru>
2020-10-12 23:37:08 +0300

    Добавил SQL-запрос для выборки дочерних узлов дерева в PostgreSQL

---
+++ net-core/sql/Tree/PostgreSQL/Tree.Axis.Child.sql
@@ -0,0 +1,19 @@
+﻿-- Axis Child
+
+with "cte_input" as 
+(
+	select
+		"id" "root_id"
+	from
+		"public"."dummy_tree"
+	where
+		"name" = 'Name-1-1-1'
+)
+select
+	t.*
+from
+	"public"."dummy_tree" "t"
+	cross join "cte_input"
+where
+	"t"."parent_id" = "cte_input"."root_id"
+;
--- net-core/sql/Tree/SQL Server/Tree.Axis.Child.sql
+++ net-core/sql/Tree/SQL Server/Tree.Axis.Child.sql
@@ -1,11 +1,19 @@
 ﻿-- Axis Child
 
-declare @RootId bigint = 2;
-
+with cte_Input as 
+(
+	select
+		Id RootId
+	from
+		dbo.DummyTree
+	where
+		[Name] = 'Name-1-1-1'
+)
 select
 	t.*
 from
 	dbo.DummyTree t
+	cross join cte_Input
 where
-	t.ParentId = @RootId
+	t.ParentId = cte_Input.RootId
 ;
